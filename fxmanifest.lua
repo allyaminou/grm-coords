1c225708d266dca2c1af4ff57c959085b2b8a72e
JvstDev <148901624+JvstDev@users.noreply.github.com>
2023-10-24 20:51:28 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,15 @@
+--[[ FX Info ]]--
 fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
+--[[ Resource Info ]]--
+author 'Jv$t'
+description 'Standalone CopyCoords'
+version '0.0.1'
+repository 'https://github.com/JvstDev/grm-coords'
+
+--[[ Manifest Info ]]--
 ui_page 'nui.html'
 
 client_scripts {
